b8193085cec71d742589b9790833137590b9bf05
Oles Leskiv <oles.leskiv@teonica.com>
2020-02-17 12:23:33 +0200

    refactoring

--- src/containers/List/index.js
+++ src/containers/List/index.js
@@ -6,85 +6,141 @@ import ListItem from './components/ListItem/ListItem';
 import './styles.css';
 
 const ITEMS_IN_VIEW = 5;
+const LIST_SIZE = 10;
 
 const List = ({ getBeerList, list = [], loading }) => {
-  const [lastIndex, setLastIndex] = useState([0, 10]);
   const [pageNumber, setPageNumber] = useState(1);
   const [lastElement, setLastElement] = useState(null);
   const [firstElement, setFirstElement] = useState(null);
-  const [renderedList, setRenderList] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+
+  const [topSentinelPreviousY, setTopSentinelPreviousY] = useState(0);
+  const [topSentinelPreviousRatio, setTopSentinelPreviousRatio] = useState(0);
+  const [bottomSentinelPreviousY, setBottomSentinelPreviousY] = useState(0);
+  const [
+    bottomSentinelPreviousRatio,
+    setBottomSentinelPreviousRatio
+  ] = useState(0);
+
+  const topSentinelPreviousYRef = useRef(topSentinelPreviousY);
+  const topSentinelPreviousRatioRef = useRef(topSentinelPreviousRatio);
+  const bottomSentinelPreviousYRef = useRef(bottomSentinelPreviousY);
+  const bottomSentinelPreviousRatioRef = useRef(bottomSentinelPreviousRatio);
+
+  useEffect(() => {
+    topSentinelPreviousYRef.current = topSentinelPreviousY;
+    topSentinelPreviousRatioRef.current = topSentinelPreviousRatio;
+    bottomSentinelPreviousYRef.current = bottomSentinelPreviousY;
+    bottomSentinelPreviousRatioRef.current = bottomSentinelPreviousRatio;
+  }, [
+    topSentinelPreviousY,
+    topSentinelPreviousRatio,
+    bottomSentinelPreviousY,
+    bottomSentinelPreviousRatio
+  ]);
+
+  const [hasMore, setHasMore] = useState(false);
 
   const listRef = useRef(list);
-  const renderedListRef = useRef(renderedList);
-
-  const getPrevListForRender = (initList, prevList) => {
-    const foundedIndex = initList.findIndex(item => {
-      const lastElem = prevList[prevList.length - 1];
-      return item.id === lastElem.id;
-    });
-
-    if (foundedIndex !== 0) {
-      setRenderList(initList.slice(foundedIndex, foundedIndex + 10));
-    }
-  };
-  const getNextListForRender = (initList, prevList) => {
-    const foundedIndex = initList.findIndex(item => {
-      const lastElem = prevList[prevList.length - 1];
-
-      return item.id === lastElem.id;
-    });
-
-    if (foundedIndex === initList.length - 1) {
-      setPageNumber(prevPageNumber => prevPageNumber + 1);
-
-      setLastIndex([
-        foundedIndex - ITEMS_IN_VIEW + 1,
-        foundedIndex + ITEMS_IN_VIEW + 1
-      ]);
-    }
-
-    setRenderList(
-      initList.slice(
-        foundedIndex - ITEMS_IN_VIEW + 1,
-        foundedIndex + ITEMS_IN_VIEW + 1
-      )
-    );
-  };
-
-  const observerLast = React.useRef(
-    new IntersectionObserver(
-      entries => {
-        const [first] = entries;
-        console.log('LAST', first);
-        if (first.isIntersecting) {
-          getNextListForRender(listRef.current, renderedListRef.current);
-        }
-      },
-      { threshold: 1 }
-    )
-  );
-
-  const observerFirst = React.useRef(
-    new IntersectionObserver(
-      entries => {
-        const [first] = entries;
-        console.log('FIRST', first);
-        if (first.isIntersecting) {
-          getPrevListForRender(listRef.current, renderedListRef.current);
-        }
-      },
-      { threshold: 1 }
-    )
-  );
 
   useEffect(() => {
     listRef.current = list;
-    renderedListRef.current = renderedList;
-  }, [list, renderedList]);
+  }, [list]);
+
+  const renderList = () => {
+    const listForRender = [];
+    const endIndex =
+      startIndex + 10 > list.length ? list.length : startIndex + 10;
+
+    for (let i = startIndex, j = 0; i < endIndex; i++, j++) {
+      const firstRef = i === startIndex ? setFirstElement : null;
+      const lastRef = i === startIndex + 10 - 1 ? setLastElement : null;
+      listForRender.push(
+        <li id={j} key={list[i].id} ref={firstRef || lastRef}>
+          {list[i].id}
+          <ListItem
+            name={list[i].name}
+            imageUrl={list[i].image_url}
+            description={list[i].description}
+          />
+        </li>
+      );
+    }
+
+    return listForRender;
+  };
+
+  const getSlidingWindow = isScrollDown => {
+    const increment = LIST_SIZE / 2;
+    let firstIndex;
+
+    if (isScrollDown) {
+      firstIndex = startIndex + increment;
+    } else {
+      firstIndex = startIndex - increment;
+    }
+
+    if (firstIndex < 0) {
+      firstIndex = 0;
+    }
+
+    return firstIndex;
+  };
+
+  const observer = React.useRef(
+    new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.target.id === '0') {
+            console.log('first', entry);
+            const currentY = entry.boundingClientRect.top;
+            const currentRatio = entry.intersectionRatio;
+            const isIntersecting = entry.isIntersecting;
+
+            console.log(currentY, currentRatio, isIntersecting);
+
+            if (
+              currentY > topSentinelPreviousYRef.current &&
+              isIntersecting &&
+              currentRatio >= topSentinelPreviousRatioRef.current &&
+              startIndex !== 0
+            ) {
+              const firstIndex = getSlidingWindow(false);
+              setStartIndex(() => firstIndex);
+
+              setTopSentinelPreviousY(() => currentY);
+              setTopSentinelPreviousRatio(() => currentRatio);
+            }
+          } else if (entry.target.id === `${LIST_SIZE - 1}`) {
+            console.log('last', entry);
+
+            const currentY = entry.boundingClientRect.top;
+            const currentRatio = entry.intersectionRatio;
+            const isIntersecting = entry.isIntersecting;
+
+            console.log(currentY, currentRatio, isIntersecting);
+
+            if (
+              currentY < bottomSentinelPreviousYRef.current &&
+              currentRatio > bottomSentinelPreviousRatioRef.current &&
+              isIntersecting
+            ) {
+              const firstIndex = getSlidingWindow(true);
+              setStartIndex(() => firstIndex);
+            }
+
+            setBottomSentinelPreviousY(() => currentY);
+            setBottomSentinelPreviousRatio(() => currentRatio);
+          }
+        });
+      },
+      { threshold: 1 }
+    )
+  );
 
   useEffect(() => {
     const currentElement = lastElement;
-    const currentObserver = observerLast.current;
+    const currentObserver = observer.current;
 
     if (currentElement) {
       currentObserver.observe(currentElement);
@@ -99,7 +155,7 @@ const List = ({ getBeerList, list = [], loading }) => {
 
   useEffect(() => {
     const currentElement = firstElement;
-    const currentObserver = observerFirst.current;
+    const currentObserver = observer.current;
 
     if (currentElement) {
       currentObserver.observe(currentElement);
@@ -116,58 +172,19 @@ const List = ({ getBeerList, list = [], loading }) => {
     getBeerList(pageNumber);
   }, [getBeerList, pageNumber]);
 
-  useEffect(() => {
-    setRenderList(list.slice(...lastIndex));
-  }, [setRenderList, list, lastIndex]);
   return (
     <div className="list__wrapper">
       <ul className="list">
-        {renderedList.map((item, index) => {
-          if (index === 0) {
-            return (
-              <li key={item.id} ref={setFirstElement}>
-                {item.id}
-                <ListItem
-                  name={item.name}
-                  imageUrl={item.image_url}
-                  description={item.description}
-                />
-              </li>
-            );
-          }
-
-          if (renderedList.length === index + 1) {
-            return (
-              <li key={item.id} ref={setLastElement}>
-                {item.id}
-                <ListItem
-                  name={item.name}
-                  imageUrl={item.image_url}
-                  description={item.description}
-                />
-              </li>
-            );
-          }
-
-          return (
-            <li key={item.id}>
-              {item.id}
-              <ListItem
-                name={item.name}
-                imageUrl={item.image_url}
-                description={item.description}
-              />
-            </li>
-          );
-        })}
+        {!!list.length && renderList()}
+        {loading && <li>Loading...</li>}
       </ul>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  list: state.list.beerList,
-  loading: state.list.isLoading
+const mapStateToProps = ({ list }) => ({
+  list: list.beerList,
+  loading: list.isLoading
 });
 
 const mapDispatchToProps = dispatch =>
